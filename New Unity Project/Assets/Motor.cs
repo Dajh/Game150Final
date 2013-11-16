560dc0d2b7a61b3088fc3c0bcc0fc86ac3fcedd3
Iceman07 <gabe_guitarded@hotmail.com>
2013-11-16 14:15:07 -0500

    0.2
    -Added script for changing object colours in inspector
    -Added multiple in-variables to motor script
    -Set aim and power lineRenderers to 1F above plane to clear onstructions
    -Bunch of other crap

--- New Unity Project/Assets/Motor.cs
+++ New Unity Project/Assets/Motor.cs
@@ -5,13 +5,18 @@ public class Motor : MonoBehaviour {
 	
 	public GameObject PowerLine;
 	public GameObject AimLine;
-	public LineRenderer powerLine;
-	public LineRenderer aimLine;
+    public float SpeedMultiplier;
+    public float Radius;
+	private LineRenderer powerLine;
+	private LineRenderer aimLine;
     private bool powerInc = true;
 	private float power = 1F;
+    private Vector3 lineHeight = new Vector3(0F, 0F, -1F);
+    private float aimLineLength = 3F;
 	
 	// Use this for initialization
 	void Start () {
+        this.gameObject.collider.material.bounciness = 1F;
 		powerLine = PowerLine.GetComponent<LineRenderer>();
 		powerLine.SetWidth(0.5f,0.5f);
 		powerLine.SetVertexCount(2);
@@ -31,17 +36,19 @@ public class Motor : MonoBehaviour {
 		float mouseDistance = (mousePos - objPos).magnitude;
 
 		#region Manages drawing of the aiming and force LineRenderers
-		if (this.gameObject.rigidbody.velocity.magnitude == 0F)
+		if (this.gameObject.rigidbody.velocity.magnitude < 0.01F)
 		{
+            Vector3 linesEnd = (mousePos - objPos).normalized;
+            this.gameObject.rigidbody.velocity = new Vector3(0F, 0F, 0F);
 			//Debug.Log(mouseDistance);
 			if (mouseDistance < 20F)
 			{
-				aimLine.SetPosition(0, objPos);
-				aimLine.SetPosition(1, objPos + (mousePos - objPos).normalized * 3);
+                aimLine.SetPosition(0, (objPos + linesEnd * Radius) + lineHeight);
+				aimLine.SetPosition(1, (objPos + linesEnd * aimLineLength) + lineHeight);
 			}
 			if (Input.GetKey(KeyCode.Mouse0))
 			{
-				powerLineVector = (mousePos - objPos).normalized * power;
+				powerLineVector = linesEnd * power;
                 if (power >= 10F || power <= 0F)
                 {
                     powerInc = !powerInc;
@@ -54,29 +61,21 @@ public class Motor : MonoBehaviour {
                 {
                     power -= 0.07F;
                 }
-				powerLine.SetPosition(0, objPos);
-				powerLine.SetPosition(1, objPos + powerLineVector);
+                powerLine.SetPosition(0, (objPos + linesEnd * Radius) + lineHeight);
+				powerLine.SetPosition(1, (objPos + powerLineVector) + lineHeight);
 			}
 			else if (Input.GetKeyUp(KeyCode.Mouse0))
 			{
-				powerLineVector = (mousePos - objPos).normalized * power;
+				powerLineVector = linesEnd * power;
 				power = 1F;
-				this.gameObject.transform.rigidbody.AddForce(powerLineVector * 120F);
-				Debug.Log("powerV: " + powerLineVector.x + ", " + powerLineVector.y);
+				this.gameObject.transform.rigidbody.AddForce(powerLineVector * SpeedMultiplier);
+				//Debug.Log("powerV: " + powerLineVector.x + ", " + powerLineVector.y);
 			}
 			else
 			{
 				powerLineVector = objPos;
 				powerLine.SetPosition(0, objPos);
 				powerLine.SetPosition(1, objPos);
-			}
-			
-			if (Input.GetKeyDown(KeyCode.Space))
-			{
-				//Debug.Log("Ball: " + objPos.normalized.x + "," + objPos.normalized.y);
-				//Debug.Log("Mouse: " + mousePos.normalized.x + "," + mousePos.normalized.y);
-				Debug.Log("powerV: " + powerLineVector.x + ", " + powerLineVector.y);
-				//Debug.Log("mDist: " + mouseDistance);
 			}
 		}
 		else
@@ -85,17 +84,24 @@ public class Motor : MonoBehaviour {
 			aimLine.SetPosition(1, objPos);
 			powerLine.SetPosition(0, objPos);
 			powerLine.SetPosition(1, objPos);
-            if (this.gameObject.rigidbody.velocity.magnitude > 0.3F)
+            if (this.gameObject.rigidbody.velocity.magnitude > 1.8F)
             {
                 this.gameObject.rigidbody.drag = 0.25F;
             }
             else
             {
-                this.gameObject.rigidbody.drag = 3F;
+                this.gameObject.rigidbody.drag = 1.9F;
             }
 		}
 		#endregion
 
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("Ball: " + objPos.normalized.x + "," + objPos.normalized.y);
+            //Debug.Log("Mouse: " + mousePos.normalized.x + "," + mousePos.normalized.y);
+            //Debug.Log("V: " + this.gameObject.rigidbody.velocity.x + ", " + this.gameObject.rigidbody.velocity.y);
+            //Debug.Log("mDist: " + mouseDistance);
+            Debug.Log("vMag: " + this.gameObject.rigidbody.velocity.magnitude);
+        }
 	}
 }
