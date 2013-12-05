3503ff1ece76bbdf83a204b095e00139736234c2
Iceman07 <gabe_guitarded@hotmail.com>
2013-12-04 21:23:16 -0500

    0.7
    -First map partially built
    -Little herps and derps here and there
    -SpeedUpSlowDown is fucked, needs to be fixed with Quaternion.AngleAxis

--- New Unity Project/Assets/SpeedUpSlowDown_old.cs
+++ New Unity Project/Assets/SpeedUpSlowDown_old.cs
@@ -6,9 +6,6 @@ public class SpeedUpSlowDown : MonoBehaviour {
     private GameObject Player;
     private bool InTrigger = false;
     public float Force = 30;
-    private float angle;
-    private Vector3 slowDown;
-    private float[] suckmydick;
 
 	// Use this for initialization
 	void Start () {
@@ -21,23 +18,16 @@ public class SpeedUpSlowDown : MonoBehaviour {
             this.gameObject.renderer.material.color = new Color(0F, 0F, 1F, 0.5F);
         }
         Player = GameObject.Find("Player");
-
-        slowDown = new Vector3(0F, Force, 0F);
-        angle = Mathf.RoundToInt(this.gameObject.transform.rotation.z) * (Mathf.PI / 180);
-        Debug.Log(angle);
-        suckmydick = new float[4] { Mathf.Cos(angle), -Mathf.Sin(angle), Mathf.Sin(angle), Mathf.Cos(angle) };
-        foreach (float asdlfkj in suckmydick)
-        {
-            Debug.Log(asdlfkj);
-        }
-        slowDown = Rotate(slowDown, suckmydick);
-        Debug.Log(slowDown.ToString());
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         if (InTrigger)
         {
+            Vector3 slowDown = new Vector3(0F, -Force, 0F);
+            float angle = this.gameObject.transform.rotation.z * (Mathf.PI / 180);
+            Quaternion rotate = new Quaternion(Mathf.Cos(angle), -Mathf.Sin(angle), Mathf.Sin(angle), Mathf.Cos(angle));
+            slowDown = rotate * slowDown;
             Player.rigidbody.AddForce(slowDown);
         }
 	}
@@ -50,10 +40,5 @@ public class SpeedUpSlowDown : MonoBehaviour {
     void OnTriggerExit()
     {
         InTrigger = false;
-    }
-
-    public Vector3 Rotate(Vector3 fuck, float[] you)
-    {
-        return new Vector3((fuck.x * you[0] + fuck.y * you[1]), (fuck.x * you[2] + fuck.y + you[3]), 0F);
     }
 }
